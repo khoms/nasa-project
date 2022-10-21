91fc2e107d42f18b0c4c5687faf2018cf04ef37f
unknown <sundp.kshetri@gmail.com>
2022-10-21 22:23:33 +0545

    Loading Data on StartUp

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,19 @@
 const http = require("http");
 
+const { loadPlanetsData } = require("./models/planets.model");
+
 const app = require("./app");
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+const runServer = async () => {
+  await loadPlanetsData();
+
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+};
+
+runServer();
