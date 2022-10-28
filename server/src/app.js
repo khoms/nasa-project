bcee9983610d8e52471e338933eb42656c801a8e
unknown <sundp.kshetri@gmail.com>
2022-10-28 19:19:08 +0545

    Working on Launches MCR

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,11 @@
+const path = require("path");
+const fs = require("fs");
 const express = require("express");
+const morgan = require("morgan");
 const cors = require("cors");
 
 const planetsRouter = require("./routes/planets.router");
+const launchesRouter = require("./routes/launches.route");
 
 const app = express();
 
@@ -10,9 +14,15 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+app.use(morgan("combined"));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use(planetsRouter);
+app.use(launchesRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
