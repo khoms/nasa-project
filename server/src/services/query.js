055e919b66352c57e1fce827b5f003a50460d1e6
unknown <sundp.kshetri@gmail.com>
2022-11-04 23:35:46 +0545

    Missing variables in query

--- server/src/services/query.js
+++ server/src/services/query.js
@@ -1,3 +1,6 @@
+const DEFAULT_PAGE_NUMBER = 1;
+const DEFAULT_PAGE_LIMIT = 200;
+
 const getPagination = (query) => {
   const page = Math.abs(query.page) || DEFAULT_PAGE_NUMBER;
   const limit = Math.abs(query.limit) || DEFAULT_PAGE_LIMIT;
